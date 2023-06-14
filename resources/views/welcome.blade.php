d392c5fc60e5290dcc37296cdfd4dd60834d75e4
Alexandros Tsikakis <Alexandros.Tsikakis@ymail.com>
2023-06-14 10:13:15 +0200

    addes some styling + unicorn gif

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,14 +4,29 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>tombraider</title>
+        <title>SSO Example</title>
 
+        {{-- bootstrap css --}}
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+        {{-- font awesome --}}
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
     </head>
-    <body>
-        <h1>SSO Login</h1>
-        {{-- login with google --}}
-        <a href="{{ route('loginsoc') }}">Login with Google</a><br><br>
-        {{-- login with github --}}
-        <a href="{{ route('logingithub') }}">Login with Github</a>
+    <body class="bg-dark">
+
+        <div class="container">
+            <div class="row justify-content-center mt-5 mb-5">
+                <div class="col-md-10 bg-light p-5 rounded text-center">
+                    <h1 class="mb-5">SSO Login</h1>
+                    {{-- login with google --}}
+                    <a href="{{ route('loginsoc') }}" class="btn btn-outline-primary">Login with Google <i class="fa-brands fa-google"></i></a><br><br>
+                    {{-- login with github --}}
+                    <a href="{{ route('logingithub') }}" class="btn btn-outline-primary">Login with Github <i class="fa-brands fa-github"></i></a><br><br>            
+                
+                    {{-- UNICORN GIF --}}
+                    <img src="https://i.gifer.com/5KTG.gif" alt="unicorn" class="mt-5">
+                </div>
+            </div>
+
     </body>
 </html>
